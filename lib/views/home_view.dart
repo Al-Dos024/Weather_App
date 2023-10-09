b5c8c431a7cb217aed5b084acb0a4c5bae42234c
Al-Dos024 <amjdos0amjdos0190edu@gmail.com>
2023-10-09 22:13:36 +0300

    add simple UI

--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: must_be_immutable
+
 import 'package:flutter/material.dart';
 import 'package:weather_app/Models/weather_model.dart';
 import 'package:weather_app/widgets/no_weather_body.dart';
@@ -29,7 +31,50 @@ class HomeView extends StatelessWidget {
               icon: Icon(Icons.search))
         ],
       ),
-      body: weatherData == null ? NoWeatherBody() : Container(),
+      body: weatherData != null
+          ? NoWeatherBody()
+          : Container(
+              color: Colors.orange,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Spacer(
+                    flex: 2,
+                  ),
+                  Text(
+                    "CityName",
+                    style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
+                  ),
+                  Text(
+                    "Time : 2-12-2003",
+                    style: TextStyle(fontSize: 24),
+                  ),
+                  Spacer(),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      Image.asset('assets/images/clear.png'),
+                      Text(
+                        "30",
+                        style: TextStyle(
+                            fontSize: 32, fontWeight: FontWeight.bold),
+                      ),
+                      Column(
+                        children: [Text('minTemp = 12'), Text('maxTemp = 30')],
+                      )
+                    ],
+                  ),
+                  Spacer(),
+                  Text(
+                    "Clear",
+                    style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
+                  ),
+                  Spacer(
+                    flex: 3,
+                  ),
+                ],
+              ),
+            ),
     );
   }
 }
