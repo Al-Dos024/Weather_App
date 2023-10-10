bfd8639994d85daa4190c9cc2a06192f791e7921
Al-Dos024 <amjdos0amjdos0190edu@gmail.com>
2023-10-10 23:34:01 +0300

    useless file

--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -4,9 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:weather_app/Models/weather_model.dart';
 import 'package:weather_app/widgets/no_weather_body.dart';
-
 import '../Providers/weather_provider.dart';
-import '../widgets/weather_info_body.dart';
 import 'Search_Page.dart';
 
 class HomeView extends StatelessWidget {
