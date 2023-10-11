a835529f534bdaef4db53ce7e34f8ef68f3190b7
Al-Dos024 <amjdos0amjdos0190edu@gmail.com>
2023-10-11 22:29:05 +0300

    just added the const

--- lib/Services/weather_service.dart
+++ lib/Services/weather_service.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: avoid_print, non_constant_identifier_names
+
 import 'dart:convert';
 
 import 'package:http/http.dart' as http;
--- lib/views/Search_Page.dart
+++ lib/views/Search_Page.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: must_be_immutable, use_build_context_synchronously, avoid_print, non_constant_identifier_names
+// ignore_for_file: must_be_immutable, use_build_context_synchronously, avoid_print, non_constant_identifier_names, file_names
 
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
